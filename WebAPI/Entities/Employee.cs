90695e610fba7cca6827bdca20c1632670b96af6
Emre Savaş <emre_savas28@hotmail.com>
2023-11-01 15:20:30 +0300

    Create Empoyee class.

---
+++ WebAPI/Entities/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Entities
+{
+    public record Employee
+    {
+        [Key]
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        public string Surname { get; init; }
+
+        [Required]
+        [MaxLength(11),MinLength(11)]
+        [RegularExpression(@"^[a-zA-Z0-9]+$")]
+        public string RegistrationNumber { get; init; }
+
+        public int? ManagerId { get; init; }
+
+        public ICollection<int>? SubordinatesIds { get; init; }
+
+
+        [ForeignKey("ManagerId")]
+        public Employee? Manager { get; init; }
+
+        [ForeignKey("SubordinatesIds")]
+        public ICollection<Employee>? Subordinates { get; init; }
+    }
+}
