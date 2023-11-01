1e074b7813dedf5e048b614869f659610acfea6c
Emre Savaş <emre_savas28@hotmail.com>
2023-11-01 17:18:15 +0300

    Create EmployeeManager.

---
+++ WebAPI/Services/Contracts/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Entities;
+
+namespace WebAPI.Services.Contracts
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAllEmployees(bool trackChanges);
+        Employee GetEmployeeById(int id, bool trackChanges);
+        bool CreateEmployee(Employee employee);
+        bool UpdateEmployee(Employee employee, bool trackChanges);
+        bool DeleteEmployee(int id, bool trackChanges);
+    }
+}
---
+++ WebAPI/Services/EmployeeManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using WebAPI.Entities;
+using WebAPI.Repositories.Contracts;
+using WebAPI.Services.Contracts;
+
+namespace WebAPI.Services
+{
+    public class EmployeeManager : IEmployeeService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+        public EmployeeManager(IRepositoryManager repositoryManager)
+        {
+            _repositoryManager = repositoryManager;
+        }
+
+        public bool CreateEmployee(Employee employee)
+        {
+            if (employee == null)
+                return false;
+
+            _repositoryManager.Employee.CreateEmployee(employee);
+            _repositoryManager.Save();
+            return true;
+        }
+
+        public bool DeleteEmployee(int id, bool trackChanges)
+        {
+            var employeeToDelete = GetEmployeeById(id, trackChanges);
+            if (employeeToDelete == null)
+                return false;
+            _repositoryManager.Employee.DeleteEmployee(employeeToDelete);
+            _repositoryManager.Save();
+            return true;
+
+        }
+
+        public IEnumerable<Employee> GetAllEmployees(bool trackChanges) 
+            => _repositoryManager.Employee.GetAllEmployees(trackChanges);
+
+        public Employee GetEmployeeById(int id, bool trackChanges) 
+            => _repositoryManager.Employee.GetEmployeeById(id, trackChanges);
+
+        public bool UpdateEmployee(Employee employee, bool trackChanges)
+        {
+            var employeeToUpdate = GetEmployeeById(employee.Id, trackChanges);
+            if (employeeToUpdate == null)
+                return false;
+            _repositoryManager.Employee.UpdateEmployee(employee);
+            _repositoryManager.Save();
+            return true;
+        }
+    }
+}
