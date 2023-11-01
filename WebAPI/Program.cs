456bec15bb12366abaaa503ce7808b49fa42acef
Emre Savaş <emre_savas28@hotmail.com>
2023-11-01 17:25:09 +0300

    Add dependency injection for IServiceManager.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,7 @@
 using WebAPI.Repositories;
 using WebAPI.Repositories.Contracts;
+using WebAPI.Services;
+using WebAPI.Services.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
 var app = builder.Build();
 
