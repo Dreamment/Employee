bbe8ea4ef5567e8404d2fae68df5cd9b50518412
Emre Savaş <emre_savas28@hotmail.com>
2023-11-01 16:47:24 +0300

    Create RepositoryBase.

---
+++ WebAPI/Repositories/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace WebAPI.Repositories
+{
+    public interface IRepositoryBase<T> where T: class
+    {
+        IQueryable<T> FindAll(bool trackchanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackchanges);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
