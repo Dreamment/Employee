d78f9c804eb2212f31f0312fd1b703cc1d9bdbae
Emre Savaş <emre_savas28@hotmail.com>
2023-11-01 16:29:44 +0300

    Create RepositoryContext.

---
+++ WebAPI/Repositories/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
+
+namespace WebAPI.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
