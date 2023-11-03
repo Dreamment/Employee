72feaed30b3457a26a0c0945587114291a623b41
Emre Savaş <emre_savas28@hotmail.com>
2023-11-04 00:59:19 +0300

    Change methods synchron to asynchron and rename.

--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -18,51 +18,51 @@ namespace WebAPI.Controllers
             _serviceManager = serviceManager;
         }
 
-        [HttpGet(Name = "GetAllEmployees")]
-        public IActionResult GetAllEmployees()
+        [HttpGet(Name = "GetAllEmployeesAsync")]
+        public async Task<IActionResult> GetAllEmployeesAsync()
         {
-            var employees = _serviceManager.Employee.GetAllEmployees(false);
+            var employees = await _serviceManager.Employee.GetAllEmployeesAsync(false);
             if (employees == null)
                 return NotFound();
             return Ok(employees);
         }
 
-        [HttpGet("{id:int}", Name = "GetEmployeeById")]
-        public IActionResult GetEmployeeById([FromRoute(Name = "id")] int id)
+        [HttpGet("{id:int}", Name = "GetEmployeeByIdAsync")]
+        public async Task<IActionResult> GetEmployeeByIdAsync([FromRoute(Name = "id")] int id)
         {
-            var employee = _serviceManager.Employee.GetEmployeeById(id, false);
+            var employee = await _serviceManager.Employee.GetEmployeeByIdAsync(id, false);
             if (employee == null)
                 return NotFound();
             return Ok(employee);
         }
 
-        [HttpPost(Name = "CreateEmployee")]
-        public IActionResult CreateEmployee([FromBody] EmployeeDtoForCreate employeeDto)
+        [HttpPost(Name = "CreateEmployeeAsync")]
+        public async Task<IActionResult> CreateEmployeeAsync([FromBody] EmployeeDtoForCreate employeeDto)
         {
             if (employeeDto == null)
                 return BadRequest("Employee object is null");
             if (!ModelState.IsValid)
                 return BadRequest("Invalid model object");
-            var Id = _serviceManager.Employee.CreateEmployee(employeeDto);
+            var Id = await _serviceManager.Employee.CreateEmployeeAsync(employeeDto);
             if (Id == null)
                 return StatusCode(StatusCodes.Status500InternalServerError, "Employee could not be saved");
             return CreatedAtRoute("GetEmployeeById", new { id = Id }, employeeDto);
         }
 
-        [HttpPut("{id:int}", Name = "UpdateEmployee")]
-        public IActionResult UpdateEmployee([FromRoute(Name = "id")] int id, [FromBody] EmployeeDtoForUpdate employeeDto)
+        [HttpPut("{id:int}", Name = "UpdateEmployeeAsync")]
+        public async Task<IActionResult> UpdateEmployeeAsync([FromRoute(Name = "id")] int id, [FromBody] EmployeeDtoForUpdate employeeDto)
         {
             if (employeeDto == null)
                 return BadRequest("Employee object is null");
             if (!ModelState.IsValid)
                 return BadRequest("Invalid model object");
-            if (_serviceManager.Employee.CheckEmployeeByRegistrationNumber(employeeDto.RegistrationNumber, false))
+            if (await _serviceManager.Employee.CheckEmployeeByRegistrationNumberAsync(employeeDto.RegistrationNumber, false))
             {
                 return BadRequest("This Registration Number is being used by another employee.");
             }
             if (employeeDto.ManagerId != null)
             {
-                var entity = _serviceManager.Employee.GetEmployeeById((int)employeeDto.ManagerId, false);
+                var entity = await _serviceManager.Employee.GetEmployeeByIdAsync((int)employeeDto.ManagerId, false);
                 if (entity == null)
                     return NotFound($"The manager with ID {employeeDto.ManagerId} could not be found.");
                 if (entity.ManagerId == id)
@@ -71,35 +71,35 @@ namespace WebAPI.Controllers
                         $" because the employee with ID {id}" +
                         $" is the manager of employee with ID {employeeDto.ManagerId}.");
             }
-            bool control = _serviceManager.Employee.UpdateEmployee(id, employeeDto, false);
+            bool control = await _serviceManager.Employee.UpdateEmployeeAsync(id, employeeDto, false);
             if (control)
                 return NoContent();
             return NotFound();
         }
 
-        [HttpDelete("{id:int}", Name = "DeleteEmployee")]
-        public IActionResult DeleteEmployee([FromRoute(Name = "id")] int id)
+        [HttpDelete("{id:int}", Name = "DeleteEmployeeAsync")]
+        public async Task<IActionResult> DeleteEmployeeAsync([FromRoute(Name = "id")] int id)
         {
-            var employee = _serviceManager.Employee.GetEmployeeById(id, false);
+            var employee = await _serviceManager.Employee.GetEmployeeByIdAsync(id, false);
             if (employee == null)
                 return NotFound();
-            _serviceManager.Employee.DeleteEmployee(id, false);
+            await _serviceManager.Employee.DeleteEmployeeAsync(id, false);
             return NoContent();
         }
 
-        [HttpPatch("{id:int}", Name = "PartiallyUpdateEmployee")]
-        public IActionResult PartiallyUpdateEmployee([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<EmployeeDtoForUpdate> employeePatch)
+        [HttpPatch("{id:int}", Name = "PartiallyUpdateEmployeeAsync")]
+        public async Task<IActionResult> PartiallyUpdateEmployeeAsync([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<EmployeeDtoForUpdate> employeePatch)
         {
             if (employeePatch == null)
                 return BadRequest("Employee object is null");
-            var employeeToUpdateDto = _serviceManager.Employee.GetEmployeeById(id, false);
+            var employeeToUpdateDto = await _serviceManager.Employee.GetEmployeeByIdAsync(id, false);
             if (employeeToUpdateDto == null)
                 return NotFound();
 
             if (employeePatch.Operations.Any(op => op.path == "managerId"))
             {
                 int newManagerId = Convert.ToInt32(employeePatch.Operations.FirstOrDefault(op => op.path == "managerId").value);
-                var entity = _serviceManager.Employee.GetEmployeeById(newManagerId, false);
+                var entity = await _serviceManager.Employee.GetEmployeeByIdAsync(newManagerId, false);
                 if (entity == null)
                     return NotFound($"Manager with ID {newManagerId} could not be found.");
                 if (entity.ManagerId == id)
@@ -108,15 +108,15 @@ namespace WebAPI.Controllers
                         $" because the employee with ID {id}" +
                         $" is the manager of employee with ID {newManagerId}.");
             }
-            if(employeePatch.Operations.Any(op => op.path == "registrationNumber"))
+            if (employeePatch.Operations.Any(op => op.path == "registrationNumber"))
             {
                 string newRegistrationNumber = employeePatch.Operations.FirstOrDefault(op => op.path == "registrationNumber").value.ToString();
-                if (_serviceManager.Employee.CheckEmployeeByRegistrationNumber(newRegistrationNumber, false))
+                if (await _serviceManager.Employee.CheckEmployeeByRegistrationNumberAsync(newRegistrationNumber, false))
                 {
                     return BadRequest("This Registration Number is being used by another employee.");
                 }
             }
-            _serviceManager.Employee.PartiallyUpdateEmployee(employeeToUpdateDto, employeePatch);
+            await _serviceManager.Employee.PartiallyUpdateEmployeeAsync(employeeToUpdateDto, employeePatch);
 
             return NoContent();
         }
