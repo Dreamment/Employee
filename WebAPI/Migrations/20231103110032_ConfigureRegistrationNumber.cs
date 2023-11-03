abcaec50f9d65b25e520df418442d2c7d1273bb8
Emre Savaş <emre_savas28@hotmail.com>
2023-11-03 14:05:53 +0300

    Configure RegistrationNumber column in DB with migration.

---
+++ WebAPI/Migrations/20231103110032_ConfigureRegistrationNumber.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    public partial class ConfigureRegistrationNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_RegistrationNumber",
+                table: "Employees",
+                column: "RegistrationNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_RegistrationNumber",
+                table: "Employees");
+        }
+    }
+}
