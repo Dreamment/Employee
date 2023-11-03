519c5e1a2aa4d55ed25051525557b089a509c46a
Emre Savaş <emre_savas28@hotmail.com>
2023-11-04 00:25:29 +0300

    Change methods synchron to asynchron in EmployeeRepository.

--- WebAPI/Repositories/Contracts/IEmployeeRepository.cs
+++ WebAPI/Repositories/Contracts/IEmployeeRepository.cs
@@ -5,11 +5,11 @@ namespace WebAPI.Repositories.Contracts
     public interface IEmployeeRepository
     {
         IQueryable<Employee> GetAllEmployees(bool trackchanges);
-        Employee GetEmployeeById(int id, bool trackchanges);
+        Task<Employee> GetEmployeeById(int id, bool trackchanges);
         int CreateEmployee(Employee employee);
         void UpdateEmployee(Employee employee);
         void DeleteEmployee(Employee employee);
-        List<int> GetSubordinates(int id, bool trackchanges);
-        bool CheckEmployeeByRegistrationNumber(string registrationNumber, bool trackchanges);
+        Task<List<int>> GetSubordinates(int id, bool trackchanges);
+        Task<bool> CheckEmployeeByRegistrationNumber(string registrationNumber, bool trackchanges);
     }
 }
--- WebAPI/Repositories/EmployeeRepository.cs
+++ WebAPI/Repositories/EmployeeRepository.cs
@@ -1,18 +1,19 @@
-﻿using WebAPI.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
 using WebAPI.Repositories.Contracts;
 
 namespace WebAPI.Repositories
 {
     public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     {
-        public EmployeeRepository(RepositoryContext context) : base (context)
+        public EmployeeRepository(RepositoryContext context) : base(context)
         {
 
         }
 
-        public bool CheckEmployeeByRegistrationNumber(string registrationNumber, bool trackchanges)
+        public async Task<bool> CheckEmployeeByRegistrationNumber(string registrationNumber, bool trackchanges)
         {
-            var employee = FindByCondition(e => e.RegistrationNumber.Equals(registrationNumber), trackchanges).SingleOrDefault();
+            var employee = await FindByCondition(e => e.RegistrationNumber.Equals(registrationNumber), trackchanges).SingleOrDefaultAsync();
             if (employee == null)
                 return false;
             return true;
@@ -30,11 +31,11 @@ namespace WebAPI.Repositories
         public IQueryable<Employee> GetAllEmployees(bool trackchanges)
             => FindAll(trackchanges).OrderBy(e => e.Id);
 
-        public Employee GetEmployeeById(int id, bool trackchanges)
-            => FindByCondition(e => e.Id.Equals(id), trackchanges).SingleOrDefault();
+        public async Task<Employee> GetEmployeeById(int id, bool trackchanges)
+            => await FindByCondition(e => e.Id.Equals(id), trackchanges).SingleOrDefaultAsync();
 
-        public List<int> GetSubordinates(int id, bool trackchanges)
-            => FindByCondition(e => e.ManagerId.Equals(id), trackchanges).Select(e => e.Id).ToList();
+        public async Task<List<int>> GetSubordinates(int id, bool trackchanges)
+            => await FindByCondition(e => e.ManagerId.Equals(id), trackchanges).Select(e => e.Id).ToListAsync();
 
         public void UpdateEmployee(Employee employee)
             => Update(employee);
